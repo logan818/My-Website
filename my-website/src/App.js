4a012b116c3cbb09348aec84e712ffda5b6e96a8
LoganMacBook <logan.calhoun45@gmail.com>
2022-03-31 23:46:20 -0400

    added splash screen

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import 'cirrus-ui'; 
 import Header from './Components/Header/Header';
+import SplashScreen from './Components/Splash Screen/Splash-Screen';
 
 function App() {
   return (
     <div className="App">
+      <SplashScreen />
       <Header />
     </div>
   );
---
+++ my-website/src/Components/Splash Screen/Splash-Screen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import 'cirrus-ui';
+
+function SplashScreen() {
+    return (
+        
+<div id="splash-img" class="hero fullscreen hero-img parallax-img">
+<div class="hero-body">
+    <div class="content u-text-center">
+        <h1 class="uppercase white title">Welcome to my Homepage</h1>
+        <h5 class="uppercase white sub-title faded">"My Website"</h5>
+    </div>
+</div>
+</div>
+
+    )
+};
+
+export default SplashScreen;
