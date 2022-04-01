f1729247c1549855c69e09deacec2d073b6ea8e5
LoganMacBook <logan.calhoun45@gmail.com>
2022-04-01 15:33:09 -0400

    Test #2 in progress in App.js and Index.js

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route
-} from "react-router-dom";
-import Switch from "react-dom"
+//import {
+//  BrowserRouter as Router,
+//  Route
+//} from "react-router-dom";
+//import Switch from "react-dom"
 import './App.css';
 import 'cirrus-ui'; 
 import Header from './Components/Header/Header';
-import SplashScreen from './Components/Splash Screen/Splash-Screen';
+//import SplashScreen from './Components/Splash Screen/Splash-Screen';
 
+// Shitty Test #1
+/* Testing export Default Function
 function App() {
   return (
     
-    <div className="App">
+    //remove class="App" from container div, website still worked 
+    <div>
       <div>
       <Header />
       </div>
@@ -22,3 +25,13 @@ function App() {
 }
 
 export default App;
+*/
+
+// Please fucking work test #2
+export default function App(){
+  return(
+    <div>
+      <Header />
+    </div>
+  );
+}
--- my-website/src/index.js
+++ my-website/src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+import { render } from "react-dom";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//!! DON"T REMOVE!
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+
+// connect your app to the browser's URL: import BrowserRouter and render it around your whole app
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <App />, 
+  </BrowserRouter>,
+rootElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
+
+
