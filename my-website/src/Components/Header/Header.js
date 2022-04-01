45ea420df07ae2aa20d99d26be4e40b5d08992ac
logan818 <logan.calhoun45@gmail.com>
2022-03-31 21:54:43 -0400

    added About me and Contact me buttons in header

--- my-website/src/Components/Header/Header.js
+++ my-website/src/Components/Header/Header.js
@@ -4,11 +4,25 @@ import 'cirrus-ui';
 
 function Header() {
     return (
+        //Header -> Header-brand, nav-left -> About Me, Contact Me
         <div class="header header-fixed unselectable header-animated">
+
             <div class="header-brand">
                 <div class="nav-item">
                     <h4 class="title">Logan Calhoun</h4>
                 </div>
+            </div>
+
+            <div class="nav-right">
+
+                <div class="nav-item">
+                    <a href="#">About Me</a>
+                </div>
+
+                <div class="nav-item">
+                    <a href="#">Contact Me</a>
+                </div>
+
             </div>
         </div>
     );
