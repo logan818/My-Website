2b66de867f07162a18a05a05f554ed184a8ee864
logan818 <logan.calhoun45@gmail.com>
2022-03-31 22:20:45 -0400

    Worked on header

--- my-website/src/Components/Header/Header.js
+++ my-website/src/Components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './Header.css'
 import 'cirrus-ui';
+import { FaGithub } from "react-icons/fa"
 
 function Header() {
     return (
@@ -9,20 +10,26 @@ function Header() {
 
             <div class="header-brand">
                 <div class="nav-item">
-                    <h4 class="title">Logan Calhoun</h4>
+                    <h5>Logan Calhoun</h5>
                 </div>
             </div>
 
             <div class="nav-right">
+                <div class="nav-item">
+                    <a href="#"><FaGithub size={35}/></a>
+                </div>
 
                 <div class="nav-item">
                     <a href="#">About Me</a>
                 </div>
 
                 <div class="nav-item">
-                    <a href="#">Contact Me</a>
+                    <a href="#">LinkedIn</a>
                 </div>
 
+                <div class="nav-item">
+                    <a href="#">Contact Me</a>
+                </div>
             </div>
         </div>
     );
