14a70c4dc18669464580becf44b8273c84571e8f
logan818 <logan.calhoun45@gmail.com>
2022-04-02 09:54:44 -0400

    get button to work Fixes #1

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-//import {
-//  BrowserRouter as Router,
-//  Route
-//} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
 //import Switch from "react-dom"
 import './App.css';
 import 'cirrus-ui'; 
 import Header from './Components/Header/Header';
 import SplashScreen from './Components/Splash Screen/Splash-Screen';
+import Home from "./Pages/Home"
 
 // Shitty Test #1
 /* Testing export Default Function
@@ -28,13 +29,36 @@ export default App;
 */
 
 // Please fucking work test #2
+/* Nope
 export default function App(){
   return(
     <div>
       <Header />
 
-      /* I want this splash screen to change depending on the button click in the header */
+       I want this splash screen to change depending on the button click in the header 
+      router oversees the Components defined by the routes
+      <Router>
+        
+      </Router>
       <SplashScreen />
     </div>
   );
 }
+*/
+
+//Take #3
+
+const App = () =>{
+  return(
+    <div>
+      <Router>
+          <link to ='/'>Home</link>        
+
+          <Route exact path='/' component={Home}/>
+      </Router>
+
+    </div>
+  )
+}
+
+export default App
---
+++ my-website/src/Pages/Home.js
@@ -0,0 +1,9 @@
+import React,{useState} from 'react';
+
+import SearchRes from './results';
+import {BrowserRouter as Router,Route, Redirect}from 'react-router-dom';
+
+const Home = () =>{}
+
+
+export default Home;
